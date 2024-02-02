c1eca694268ed7b9bd9c775f7229ae4386c72663
Bridger Tower <bridgertower@gmail.com>
2024-02-02 15:00:48 -0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 export { Layout, Main, Section, Container, Article } from "./components/layout";
+export * from "./components/theme/theme-toggle";
+export * from "./components/theme/theme-provider";
